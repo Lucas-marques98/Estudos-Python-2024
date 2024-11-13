43d220fcc91c61d949e7de20df81dc72bc8873dd
Lucas Marques <lucas.rodrigues.caslu@gmail.com>
2024-11-12 22:20:15 -0300

    Segundo commit

--- Variaveis.py
+++ Variaveis.py
@@ -8,3 +8,18 @@ print(f'{velocidade_maxima}')
 Frase_de_boas_vindas = 'Olá seja bem vindo' #String - Texto
 
 print(f'{Frase_de_boas_vindas}')
+
+posso_entrar_na_sua_casa = True #True ou False - verdadeiro ou falso - Boleanos
+
+print(f'{posso_entrar_na_sua_casa}')
+
+valor_dollar = 5.25 #Decimal - Float
+
+print(type(velocidade_maxima))
+print(type(Frase_de_boas_vindas))
+print(type(posso_entrar_na_sua_casa))
+print(type(valor_dollar))
+
+velocidade,frase,posso_ou_nao = 200, "Desistir jamais", True #Atribuindo valores a variáveis na mesma linha
+
+print(f'{velocidade,frase,posso_ou_nao}')
